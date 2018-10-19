3954d183ce83c1030bfd8fc270eb379c1a642c1f
slim <smedini@gmail.com>
2018-10-19 16:53:06 +0200

    correction env

--- src/env.js
+++ src/env.js
@@ -4,7 +4,7 @@ import {version} from './../package.json'
 const ENV = {
     REST_PATH: process.env.REST_PATH || "rest",
     
-    DB_CONNECTION_STRING: "",
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_PORT: process.env.DB_PORT || 27017,
