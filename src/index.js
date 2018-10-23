ee89847c532cfd3c6900f8b52c5c1ccf68b3897f
slim <smedini@gmail.com>
2018-10-23 10:16:40 +0200

    con string

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const debug = require('debug')('api:search:alim')
 const auth = ENV => (ENV.DB_USER && ENV.DB_PWD) ? (ENV.DB_USER + ":" + ENV.DB_PWD + "@") : ""
 let connString = ENV.DB_CONNECTION_STRING ? ENV.DB_CONNECTION_STRING : `mongodb://${auth(ENV)}${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME}?authSource=admin`
 
-debug("conncting to %o", connString)
+debug("connecting to %o", connString)
 
 mongodb.MongoClient.connect(connString, {useNewUrlParser: true})
     .then(client => client.db(ENV.DB_NAME))
