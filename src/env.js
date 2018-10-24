81eee076aa58d2300d0a527b346364a8498ba00d
slim <smedini@gmail.com>
2018-10-24 11:09:43 +0200

    con string pas de valeur par défaut

--- src/env.js
+++ src/env.js
@@ -4,7 +4,7 @@ import {version} from './../package.json'
 const ENV = {
     REST_PATH: process.env.REST_PATH || "rest",
     
-    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || "mongodb://doudou:masta@mongodb-replicaset.mongodb-replicaset.svc.cluster.local:27017/BlueForestTrees?replicaSet=rs0&authSource=admin",
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_PORT: process.env.DB_PORT || 27017,
